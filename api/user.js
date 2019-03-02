b2ec574e5e02f22f263a1c6fe30b60568a19c6ad
Carlos Junior <juniorkojak2@gmail.com>
2019-03-02 16:39:58 -0300

    first commit

---
+++ api/user.js
@@ -0,0 +1,22 @@
+const bcript = require('bcrypt-nodejs')
+
+module.exports = app => {
+    const obterHash = (password, callback) => {
+        bcript.genSalt(10, (error, salt) => {
+            bcript.hash(password, salt, null, (error, hash) => callback(hash))
+        })
+    }
+
+    const save = (req, res) => {
+        obterHash(req.body.password, hash => {
+            const password = hash
+            
+            app.db('users')
+                .insert({ name: req.body.name, email: req.body.email, password })
+                .then(_ => res.status(204).send())
+                .catch(error => res.status(400).json(error))
+        })
+    }
+
+    return { save }
+}
